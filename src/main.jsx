6354f655f5b465143d0d0d2af2d78d0f95bb60f6
vitalik.nozhenko@gmail.com <vitalik.nozhenko@gmail.com>
2023-08-06 11:00:21 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
-import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './redux/store.js';
-import { Provider } from 'react-redux';
+import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-
-      <BrowserRouter basename="nova-poshta">
+    <BrowserRouter basename="/nova-poshta">
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </BrowserRouter>
-</Provider>
-)
+  </Provider>
+);

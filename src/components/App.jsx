a823efd82fa714d46c87f345d1b4f9eb9cfbf84b
vitalik.nozhenko@gmail.com <vitalik.nozhenko@gmail.com>
2023-08-06 10:48:12 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import Layout from "./Layout/Layout"
-import TtnPage from "../pages/Ttn/TtnPage"
-import DepartmentsPage from "../pages/Departments/DepartmentsPage"
-
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout/Layout";
+import TtnPage from "../pages/Ttn/TtnPage";
+import DepartmentsPage from "../pages/Departments/DepartmentsPage";
 
 const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<TtnPage />} />
+          <Route path="departments" element={<DepartmentsPage />} />
+          <Route path="*" element={<TtnPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
-  return <>
-        <Routes>
-            <Route path="/" element={<Layout/>}>
-              <Route index element={<TtnPage />} />
-              <Route path="departments" element={<DepartmentsPage />} />
-              <Route path="*" element={<TtnPage />} />
-             </Route>
-             
-              </Routes>
- 
- </>
-  
-}
-
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,90 +1,111 @@
-import { AppBar, Box, Button, Container, IconButton, Menu, MenuItem } from '@mui/material';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 
-const Header =()=>{
-    const [anchorEl, setAnchorEl] = useState(null);
+const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    const handleMenuClick = event => {
-        setAnchorEl(event.currentTarget);
-      };
-    
-      const handleMenuClose = () => {
-        setAnchorEl(null);
-      };
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    return <>
-      <AppBar position="static"
-    sx={{  backgroundColor:"#f6f6f9", width:"100%", 
-    height:"70px" , 
-}}  
-    >
-      <Container  sx={{display:'flex',  alignItems: 'center' }}>
-  <Box   component={Link}
-  to="/"
-  sx={{
-    display: 'flex',
-    flexGrow: 1,
-    textDecoration: 'none',
-    pt:"20px",pb:"20px"
-  }}
->
-      <img src="logo.png" alt="poshta" width='30px' height='30px' />
-        </Box>
-        <Box
-                sx={{
-                    flexGrow: 1,
-                    display: { xs: 'none', md: 'flex', gap: 10 },
-                }}>
-      <Button
-     variant="outlined"  
-      component={Link}
-     to='/'
-         sx={{
-     my: 2,
-     color: "rgb(211, 47, 47)",
-     borderColor: "rgb(211, 47, 47)",
-      "&:hover":{
-        borderColor: "rgb(211, 47, 47)",
-       }
-    }}>Пошук за  TTH</Button>
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
-     <Button
-     variant="outlined"  
-      component={Link}
-      to="/departments"
-         sx={{
-         my: 2,
-          color: "rgb(211, 47, 47)",
-        borderColor: "rgb(211, 47, 47)",
-        "&:hover":{
-            borderColor: "rgb(211, 47, 47)",
-          }
-      }} >Пошук відділень</Button>
-                  </Box>
-                  <IconButton
-                sx={{ display: { md: 'none' } }}
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                onClick={handleMenuClick}
-              >
-                <MenuIcon sx={{fontSize:"40px", color:"rgb(211, 47, 47)"}}/>
-              </IconButton>
-    </Container>
-     </AppBar>
-     <Menu
+  return (
+    <>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "#f6f6f9", width: "100%", height: "70px" }}
+      >
+        <Container sx={{ display: "flex", alignItems: "center" }}>
+          <Box
+            component={Link}
+            to="/"
+            sx={{
+              display: "flex",
+              flexGrow: 1,
+              textDecoration: "none",
+              pt: "20px",
+              pb: "20px",
+            }}
+          >
+            <img src="logo.png" alt="poshta" width="30px" height="30px" />
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex", gap: 10 },
+            }}
+          >
+            <Button
+              variant="outlined"
+              component={Link}
+              to="/"
+              sx={{
+                my: 2,
+                color: "rgb(211, 47, 47)",
+                borderColor: "rgb(211, 47, 47)",
+                "&:hover": {
+                  borderColor: "rgb(211, 47, 47)",
+                },
+              }}
+            >
+              Пошук за TTH
+            </Button>
+
+            <Button
+              variant="outlined"
+              component={Link}
+              to="/departments"
+              sx={{
+                my: 2,
+                color: "rgb(211, 47, 47)",
+                borderColor: "rgb(211, 47, 47)",
+                "&:hover": {
+                  borderColor: "rgb(211, 47, 47)",
+                },
+              }}
+            >
+              Пошук відділень
+            </Button>
+          </Box>
+          <IconButton
+            sx={{ display: { md: "none" } }}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleMenuClick}
+          >
+            <MenuIcon sx={{ fontSize: "40px", color: "rgb(211, 47, 47)" }} />
+          </IconButton>
+        </Container>
+      </AppBar>
+      <Menu
         anchorEl={anchorEl}
         open={Boolean(anchorEl)}
         onClose={handleMenuClose}
       >
-        <MenuItem   component={Link}  to='/' onClick={handleMenuClose}>Пошук за  TTH</MenuItem>
-        <MenuItem    component={Link} to="/departments" onClick={handleMenuClose}>Пошук відділень</MenuItem>
+        <MenuItem component={Link} to="/" onClick={handleMenuClose}>
+          Пошук за TTH
+        </MenuItem>
+        <MenuItem component={Link} to="/departments" onClick={handleMenuClose}>
+          Пошук відділень
+        </MenuItem>
       </Menu>
-     
     </>
-}
+  );
+};
 
 export default Header;
